0261e49dc0c048d6ee59aabcf741c7314327d760
John <mjhui90@gmail.com>
2019-09-19 14:06:21 -0400

    Added catch on getAllMovies to send back []

--- server/api/movies.js
+++ server/api/movies.js
@@ -3,7 +3,10 @@ const {Movie} = require('../db/models')
 const {getAllMovies} = require('../db/models/movie')
 
 router.get('/', async (req, res, next) => {
-  const movies = await getAllMovies()
+  const movies = await getAllMovies().catch(() => {
+    console.error('unable to retrieve movies')
+    return []
+  })
   res.json(movies)
 })
 

0afe54dee379487d944992f22c4b24da35f2d6f5
John <mjhui90@gmail.com>
2019-09-19 15:12:36 -0400

    removed console.log

--- client/components/views/AllMoviesView.js
+++ client/components/views/AllMoviesView.js
@@ -3,7 +3,6 @@ import {Card, CardHeader, CardMedia} from '@material-ui/core'
 import generateMovieCards from './helperViews/generateMovieCards'
 
 const AllMovieView = props => {
-  console.log(props.allMovies)
   const cards = props ? generateMovieCards(props.allMovies) : null
   return <div>{cards}</div>
 }
